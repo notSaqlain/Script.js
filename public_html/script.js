d327505017a30daa54fd67a6f849d72acf225e54
notSaqlain <iamnotsaqlain@gmail.com>
2025-01-13 09:26:27 +0100

    feat: Add interactivity to divs and spans

--- public_html/script.js
+++ public_html/script.js
@@ -4,7 +4,6 @@
  */
 
 
-// Funzione per cambiare il colore di un div quando il mouse passa sopra
 document.querySelectorAll('div').forEach(div => {
     div.addEventListener('mouseenter', () => {
         if (div.style.color === 'red') {
@@ -14,7 +13,6 @@ document.querySelectorAll('div').forEach(div => {
         }
     });
 
-    // Funzione per cambiare il colore di un div quando il mouse esce da esso
     div.addEventListener('mouseleave', () => {
         if (div.style.color === 'red') {
             div.style.color = '';
@@ -22,26 +20,23 @@ document.querySelectorAll('div').forEach(div => {
     });
 });
 
-// Funzione per cambiare il colore a blu quando si clicca su un elemento di classe "c1"
 document.querySelectorAll('.c1').forEach(element => {
     element.addEventListener('click', () => {
         element.style.color = 'blue';
     });
 
-    // Funzione per far sparire l'elemento di classe "c1" al doppio click
     element.addEventListener('dblclick', () => {
         element.style.display = 'none';
     });
 });
 
-// Funzione per cambiare il colore a verde quando si clicca su uno span di classe "c2"
 document.querySelectorAll('span.c2').forEach(span => {
     span.addEventListener('click', () => {
         span.style.color = 'green';
     });
 });
 
-// Funzione per far apparire un messaggio di allerta quando si clicca su un div con classi "c1" e "c2"
+
 document.querySelectorAll('div.c1.c2').forEach(div => {
     div.addEventListener('click', () => {
         alert('Hai cliccato su un div con entrambe le classi c1 e c2');
